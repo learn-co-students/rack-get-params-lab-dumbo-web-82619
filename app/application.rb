fbc46a145ad55bf5aedd4d377b36d084d35c10b3
Belle P <bellepx3@gmail.com>
2019-09-15 22:44:27 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,8 +1,9 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
-
-  def call(env)
+  @@cart = []
+  
+   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
@@ -13,6 +14,22 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+      if @@cart.empty?
+        resp.write "Your cart is empty"
+      else
+         @@cart.each do |item|
+          resp.write "#{item}\n"
+        end
+      end
+    elsif req.path.match(/add/)
+      item_to_add = req.params["item"]
+      if @@items.include? item_to_add 
+        @@cart << item_to_add
+        resp.write "added #{item_to_add}"
+      else
+        resp.write "We don't have that item!"
+      end
     else
       resp.write "Path Not Found"
     end
@@ -28,3 +45,46 @@ class Application
     end
   end
 end
+
+#   def call(env)
+#     resp = Rack::Response.new
+#     req = Rack::Request.new(env)
+
+#     if req.path.match(/items/)
+#       @@items.each do |item|
+#         resp.write "#{item}\n"
+#       end
+#     elsif req.path.match(/search/)
+#       search_term = req.params["q"]
+#       resp.write handle_search(search_term)
+      
+#   elsif req.path.match(/cart/)
+#   if @@cart.empty?
+#         resp.write "Your cart is empty"
+#       else
+#         resp.write @@cart
+#       end
+#     end
+#     elsif req.path.match(/add/)
+#     add_term = req.params["item"]
+#     if @@items.include?(add_term)
+#       @@cart << add_term
+#       resp.write "added #{add_term}"
+#     else 
+#       resp.write "We don't have that item!"
+#     end
+#     else
+#       resp.write "Path Not Found"
+#     end
+
+#     resp.finish
+#   end
+
+#   def handle_search(search_term)
+#     if @@items.include?(search_term)
+#       return "#{search_term} is one of our items"
+#     else
+#       return "Couldn't find #{search_term}"
+#     end
+#   end
+# end
